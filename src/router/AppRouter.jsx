75c13f2f9d5edfe6e1af29b8771c44caeb08fef4
PabloFC <pablofc_68@hotmail.com>
2024-05-13 19:55:55 +0200

    cambios en el index.html

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import MarvelPage from "../heroes/pages/MarvelPage";
 import LoginPage from "../auth/pages/LoginPage";
-import DcPage from "../heroes/pages/DCPage";
+import DcPage from "../heroes/pages/DcPage";
 
 const AppRouter = () => {
   return (
