533e6147d6ce9ecc4d7a992e20071f1ea9ec9c05
PabloFC <pablofc_68@hotmail.com>
2024-06-11 00:35:17 +0200

    Rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../auth/pages/LoginPage";
 import HeroesRoutes from "../heroes/routes/HeroesRoutes";
+import PrivateRouter from "../heroes/routes/PrivateRouter";
+import PublicRouter from "../heroes/routes/PublicRouter";
 
 const AppRouter = () => {
   return (
     <div>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route
+          path="login"
+          element={
+            <PublicRouter>
+              <LoginPage />
+            </PublicRouter>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRouter>
+              <HeroesRoutes />
+            </PrivateRouter>
+          }
+        />
       </Routes>
     </div>
   );
